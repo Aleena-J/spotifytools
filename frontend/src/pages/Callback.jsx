374e5a7986556a4205814879dca64b7dae75dd06
Aleena <aleena.james@ufl.edu>
2026-03-07 13:57:10 -0500

    fix render url

--- frontend/src/pages/Callback.jsx
+++ frontend/src/pages/Callback.jsx
@@ -7,7 +7,7 @@ function Callback() {
         const code = params.get("code");
 
         if (code) {
-            fetch("https://spotifytools-ver.vercel.app/callback", {
+            fetch("https://spotifytools.onrender.com/callback", {
                 method: "POST",
                 credentials: "include",
                 headers: {
