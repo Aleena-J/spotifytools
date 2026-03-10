3bdcd17e7333dddb7255cf238899b8e46ef2912c
Aleena <aleena.james@ufl.edu>
2026-03-09 21:03:03 -0400

    demo vid on top

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -17,14 +17,14 @@ function Home() {
 
     return (
         <div className="welcome-body">
-            <h1 className='welcome-text'>Welcome!</h1>
-            <img src={gif} className='kendall-gif'/>
-            {isLoggedIn ? <h2 className='loginText'>You are logged in!</h2> :  <h2 className='loginText'>Log in to get started!</h2>}
-            <br></br>
-            <hr></hr>
             <p>3/9/2026 - As I am not currently paying for Spotify Premium, I no longer have access to the Spotify Web API, making this website nonfunctional</p>
             <p>Below is a video demo of some of the website features in action</p>
             <iframe width="560" height="315" src="https://www.youtube.com/embed/Fa05ZBFatwk?si=k8GsRkuHCu-bnXTI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+            <br></br>
+            <hr></hr>
+            <h1 className='welcome-text'>Welcome!</h1>
+            <img src={gif} className='kendall-gif'/>
+            {isLoggedIn ? <h2 className='loginText'>You are logged in!</h2> :  <h2 className='loginText'>Log in to get started!</h2>}
         </div>
     );
 }
